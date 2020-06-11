fb53a34704226a6fb9c1cedefc0da3668aa098c9
FranciscoN3to <franciscojf@gmail.com>
2020-06-11 00:52:01 -0300

    update index app

--- resources/js/app/index.js
+++ resources/js/app/index.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-    return (
-        <div className="container">
-            <h3>Relatório gerencial</h3>
-            <button type="button" name="" id="" className="btn btn-success mr-2">
-                <i className="fas fa-filter d-inline mr-2"></i>
-                <p className="m-0 d-inline">Filtro</p>
-            </button>
-            <button type="button" name="" id="" className="btn btn-success mr-2">
-                <i className="fas fa-plus d-inline mr-2"></i>
-                <p className="m-0 d-inline">Inserir macro</p>
-            </button>
-            <button type="button" name="" id="" className="btn btn-success mr-2">
-                <i className="fas fa-share-square d-inline mr-2"></i>
-                <p className="m-0 d-inline">Exportar</p>
-            </button>
-            <div className="d-flex justify-content-center container-relatorio mt-10 rounded">
-                <div className="w-100 d-flex container justify-content-center align-items-center">
-                    <div>
-                        <i className="fas fa-info-circle d-inline text-secondary mr-2"></i>
-                        <p className="m-0 d-inline text-secondary">Sem dados para mostrar</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export default App;
-
+import App from './src'
+ 
 if (document.getElementById('app')) {
     ReactDOM.render(<App />, document.getElementById('app'));
 }
-
- 
