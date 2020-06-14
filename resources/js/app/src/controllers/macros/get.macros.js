7688916e35c90d009aa58e0e503895b2d34ecf7a
FranciscoN3to <franciscojf@gmail.com>
2020-06-14 00:35:30 -0300

    controller macros react js

---
+++ resources/js/app/src/controllers/macros/get.macros.js
@@ -0,0 +1,10 @@
+//get macros
+import store from '../../store'
+import { addMacro } from '../../actions/macros'
+import { api } from '../../services/api'
+
+function getMacrosByPeriodo(periodo, employee) {
+
+    api.get('api/')
+
+}
